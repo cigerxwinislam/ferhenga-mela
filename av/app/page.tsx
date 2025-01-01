eaa15ae0d14523f3eabd3d05729f95bbbde8b9d1
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2025-01-01 16:08:11 +0300

    Update page.tsx

--- av/app/page.tsx
+++ av/app/page.tsx
@@ -15,47 +15,45 @@ function page() {
       <Container>
       <Alert>
   
-  <Alert style={{background:"white"}}>Hayatınıza değer verin..</Alert>  
+  <Alert style={{background:"white"}}>Tüm sorularınız için çağrı merkezimizden destek alabilirsiniz. Yeni projeler ve geliştirmeler üzerinde çalışarak üretmeye devam ediyoruz.</Alert>  
   
-  <Alert style={{background:"white"}}>Ehliyet Kursu</Alert>  
+  <Alert style={{background:"white"}}>Sürücü video eğitimleri ile sizler için, anlamadığınız her noktada uzman hocalarımız ile destek veriyoruz. Özel hocalarımız ile e-sınav yap uygulaması üzerinden kursiyerlere video eğitimler düzenleyip konuları daha verimli bir şekilde idrak etmelerini sağlayarak, sınavlarından kolaylıkla geçmelerini sağladık. Sizler de hemen e-sınav yap uygulamasına dahil olarak belgelerinizi kolaylıkla alabilirsiniz.</Alert>  
     </Alert>  
 <Row>
 
 <Col>  
 
-<KitchenSinkExample nav="Kaliteli Eğitim" wene={mmwene}  nivis="Bir inşaatın bütün mimarisi..."/>
+<KitchenSinkExample nav="Kaliteli Eğitim" wene="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.surucukurslari.com%2Fhaber%2Fsurucu-kursu-nasil-acilir&psig=AOvVaw0JU_V9quXoNmyoScKh4pUH&ust=1735822841740000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLiTjZPK1IoDFQAAAAAdAAAAABAE"  nivis="Ehliyet Kursları"/>
 
-<KitchenSinkExample nav="Mobilya Tasarımı" wene={mmwene} />
+<KitchenSinkExample nav="Direksiyon Dersleri" wene="https://www.google.com/url?sa=i&url=https%3A%2F%2Fzaferyolu.com.tr%2F&psig=AOvVaw0JU_V9quXoNmyoScKh4pUH&ust=1735822841740000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLiTjZPK1IoDFQAAAAAdAAAAABAh" />
 </Col>
 <Col>  
 
-<KitchenSinkExample nav ="İç Mimar-Dekorasyon" wene={mmwene}/>
+<KitchenSinkExample nav ="Tecrübeli Hocalarımız" wene="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffinans.mynet.com%2Fhaber%2Fdetay%2Fekonomi%2Fehliyet-almanin-bedeli-artiyor-2025-yilinda-surucu-kursu-ve-ehliyet-harc-fiyatlarina-buyuk-zam-geliyor-bu-tarihi-kacirmayin%2F491981%2F&psig=AOvVaw0JU_V9quXoNmyoScKh4pUH&ust=1735822841740000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLiTjZPK1IoDFQAAAAAdAAAAABAx"/>
 
-<KitchenSinkExample nav="Genel Yaklaşım" wene={mmwene}/>
+<KitchenSinkExample nav="Birebir Dersler" wene="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ehliyet.info%2Fzonguldak-en-iyi-surucu-kurslari-204.html&psig=AOvVaw0JU_V9quXoNmyoScKh4pUH&ust=1735822841740000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLiTjZPK1IoDFQAAAAAdAAAAABAR"/>
 </Col>
 <Alert>
   
-<Alert></Alert>  
+<Alert>Online olarak ders kitaplarını inceleyebilir sonrasında sistem üzerinden testler çözebilirsiniz. Bugüne kadar Türkiye'de bir çok sürücü kursuna, operatörlük kurslarına ve src kurslarına eğitim kitapları gönderdik. Şimdi bu göndermiş olduğumuz kitapların tamamını dijital ortama aktararak; hem kurslara hem de kursiyerlere kolaylık sağladık. Sizlerde bu kolaylıklardan faydalanmak için hemen e-sınav yap uygulamasına geçiş yapın.</Alert>  
 
 <Alert></Alert>  
   </Alert>  
   <Col>  
 
-<KitchenSinkExample/>
+<KitchenSinkExample wene="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffinans.mynet.com%2Fhaber%2Fdetay%2Fekonomi%2Fehliyet-almanin-bedeli-artiyor-2025-yilinda-surucu-kursu-ve-ehliyet-harc-fiyatlarina-buyuk-zam-geliyor-bu-tarihi-kacirmayin%2F491981%2F&psig=AOvVaw0JU_V9quXoNmyoScKh4pUH&ust=1735822841740000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLiTjZPK1IoDFQAAAAAdAAAAABAx"/>
 
-<KitchenSinkExample/>
+<KitchenSinkExample wene="http://www.muglamentesesurucukursu.com/wp-content/uploads/2018/02/photo-15.jpg"/>
 </Col>
-<Col>  
 
-<KitchenSinkExample/>
-
-<KitchenSinkExample/>
-</Col>
 <Alert>
-  
-<Alert> Açıklama</Alert>  
 
-<Alert> mm ..Açıklama.....</Alert>  
+  
+<Alert>2000'in üzerinde soru bankası ile sizlerin sınava daha iyi hazırlanabilmesi için gerçeğe en yakın sınav ortamı sağlıyor, eğitim videolarımız ve online sürücü kitaplarımız ile bilgilerinizi tazeliyoruz. Çıkmış Ehliyet Soruları, Özel hazırlanan sorular, kitaplardan derlenen sorular ile sınava çok iyi hazırlanmak için vakit kaybetmeyin. Aynı zamanda İş Makineleri - Operatörlük ve SRC sınavlarına girecek adayların da çok iyi bir şekilde hazırlanabilmesini sağlamaktayız. </Alert>  
+
+<Alert> Sizin için geliştirmiş olduğumuz sürücü eğitimleri ile sınavlarınızı kolaylıkla verebilirsiniz. Online Eğitim yaşantısında teknolojinin gelişmesiyle ve pandemi süreci ile birlikte birçok yenilik meydana geldi. Online eğitim bu yeniliklerden birisidir. E-Sınav Yap olarak değişen ve gelişen zamana göre sizler için e-sınav uygulamasını hazırladık. Çalışıyor olmak, evlilik, çocuk sahibi olmak ya da diğer nedenlerden dolayı kurslardaki eğitimlere gelemeyenler için online eğitimlere katılmak büyük avantaj sağlayacaktır. Artık zamandan ve mekandan bağımsız olarak istediğiniz her an eğitimlerinizi alabilirsiniz. Online Eğitimin Avantajları Nelerdir? Zaman ve mekandan bağımsız olduğu için sadece kurslarda ders alma imkanını ortadan kaldırır ve sürekli eğitim imkanı sağlar. Çalışıyor olmak, evlilik, çocuk sahibi olmak ya da diğer nedenler uzaktan online eğitimlere katılmayı engellemez. Her bir öğrenenin kendi hızında öğrenmesine imkan sağlar. Öğretim ortamı ve ders materyallerine sınırsız erişim sağlar. Ekonomik ve tekrar kullanılabilir ders materyalleri ile ortak ders içeriği sunar.
+
+</Alert>  
   </Alert>  
 
 </Row>
